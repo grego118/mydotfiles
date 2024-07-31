8ee114343265aa44612bd9477c81ca12483a3336
Austin Gregory <austinggregory@gmail.com>
2024-07-30 20:16:52 -0400

    Update provision script

--- .provision-mac.zsh
+++ .provision-mac.zsh
@@ -15,7 +15,7 @@ xcodebuild -runFirstLaunch
 
 
 # Homebrew
-(( ! $+commands[brew] )) && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+(( ! $+commands[brew] )) && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
 brew upgrade
@@ -24,30 +24,31 @@ brew upgrade
 # General stuff
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-brew install --cask iterm2
-brew install --cask arc
-
-brew install zsh-completion powerlevel10k
+brew install powerlevel10k
 if ( ! compaudit ); then
     compaudit | xargs chmod g-w
 fi
+
+brew install zsh-completion fzf
 
 brew tap zackelia/formulae
 brew install bclm
 sudo bclm write 80
 
+brew install --cask iterm2
+
 
 # Dev tools
-brew install git go python3 pipx ripgrep fzf jq
+brew install git go python3 pipx cmake
 
-brew install gnupg ykman pinentry-mac
+brew install jq
 
-brew install colima kubectl k9s
+brew install gnupg pinentry-mac
+
+brew install docker colima kubectl k9s tilt helm
 colima nerdctl install
 
 brew install --cask google-cloud-sdk
-brew install --cask dbeaver-community
-brew install --cask jetbrains-toolbox
 
 
 # Neovim
